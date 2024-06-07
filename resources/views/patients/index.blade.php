f6f0208a1e00347de58f146dd8062fd86ed1592b
Adrian Smallwood <smallwoodadrian54@gmail.com>
2024-06-06 22:47:39 -0700

    idk

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{@extends('layouts.app')
 
 @section('content')
 
@@ -8,7 +8,8 @@
     <div class="col-md-6">
         <form action="{{ route('patients.index') }}" method="GET" class="form-inline">
             <div class="form-group">
-                <input type="text" name="search" class="form-control" placeholder="Search patients..." value="{{ request('search') }}">
+                <input type="text" name="search" class="form-control" placeholder="Search patients..."
+                       value="{{ request('search') }}">
             </div>
             <button type="submit" class="btn btn-primary ml-2">Search</button>
         </form>
@@ -22,7 +23,8 @@
 
 <hr>
 
-<div class="modal fade" id="addPatientModal" tabindex="-1" role="dialog" aria-labelledby="addPatientModalLabel" aria-hidden="true">
+<div class="modal fade" id="addPatientModal" tabindex="-1" role="dialog" aria-labelledby="addPatientModalLabel"
+     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -72,7 +74,8 @@
                         </div>
                         <div class="form-group">
                             <label for="adult_middle_name_0">Middle Name</label>
-                            <input type="text" name="adult_middle_names[]" id="adult_middle_name_0" class="form-control">
+                            <input type="text" name="adult_middle_names[]" id="adult_middle_name_0"
+                                   class="form-control">
                         </div>
                         <div class="form-group">
                             <label for="adult_last_name_0">Last Name</label>
@@ -119,16 +122,23 @@
         </td>
         <td>
             @if ($patient->status === 'resident')
-            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#dischargeModal{{ $patient->id }}">Discharge</button>
+            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#dischargeModal{{ $patient->id }}">
+                Discharge
+            </button>
             @else
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#intakeModal{{ $patient->id }}">Intake</button>
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#intakeModal{{ $patient->id }}">
+                Intake
+            </button>
             @endif
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editPatientModal{{ $patient->id }}">Edit</button>
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editPatientModal{{ $patient->id }}">
+                Edit
+            </button>
         </td>
     </tr>
 
     <!-- Patient Summary Modal -->
-    <div class="modal fade" id="patientModal{{ $patient->id }}" tabindex="-1" role="dialog" aria-labelledby="patientModalLabel{{ $patient->id }}" aria-hidden="true">
+    <div class="modal fade" id="patientModal{{ $patient->id }}" tabindex="-1" role="dialog"
+         aria-labelledby="patientModalLabel{{ $patient->id }}" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -138,7 +148,8 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p><strong>Name:</strong> {{ $patient->first_name }} {{ $patient->middle_name }} {{ $patient->last_name }}</p>
+                    <p><strong>Name:</strong> {{ $patient->first_name }} {{ $patient->middle_name }} {{
+                        $patient->last_name }}</p>
                     <p><strong>Status:</strong> {{ $patient->status }}</p>
 
                     <p><strong>Room:</strong>
@@ -189,7 +200,8 @@
     </div>
 
     <!-- Intake Modal -->
-    <div class="modal fade" id="intakeModal{{ $patient->id }}" tabindex="-1" role="dialog" aria-labelledby="intakeModalLabel{{ $patient->id }}" aria-hidden="true">
+    <div class="modal fade" id="intakeModal{{ $patient->id }}" tabindex="-1" role="dialog"
+         aria-labelledby="intakeModalLabel{{ $patient->id }}" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -217,7 +229,8 @@
     </div>
 
     <!-- Discharge Modal -->
-    <div class="modal fade" id="dischargeModal{{ $patient->id }}" tabindex="-1" role="dialog" aria-labelledby="dischargeModalLabel{{ $patient->id }}" aria-hidden="true">
+    <div class="modal fade" id="dischargeModal{{ $patient->id }}" tabindex="-1" role="dialog"
+         aria-labelledby="dischargeModalLabel{{ $patient->id }}" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -238,7 +251,8 @@
     </div>
 
     <!-- Edit Patient Modal -->
-    <div class="modal fade" id="editPatientModal{{ $patient->id }}" tabindex="-1" role="dialog" aria-labelledby="editPatientModalLabel{{ $patient->id }}" aria-hidden="true">
+    <div class="modal fade" id="editPatientModal{{ $patient->id }}" tabindex="-1" role="dialog"
+         aria-labelledby="editPatientModalLabel{{ $patient->id }}" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -248,20 +262,24 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form id="editPatientForm{{ $patient->id }}" action="{{ route('patients.update', $patient) }}" method="POST">
+                    <form id="editPatientForm{{ $patient->id }}" action="{{ route('patients.update', $patient) }}"
+                          method="POST">
                         @csrf
                         @method('PUT')
                         <div class="form-group">
                             <label for="first_name">First Name</label>
-                            <input type="text" name="first_name" id="first_name" class="form-control" value="{{ $patient->first_name }}" required>
+                            <input type="text" name="first_name" id="first_name" class="form-control"
+                                   value="{{ $patient->first_name }}" required>
                         </div>
                         <div class="form-group">
                             <label for="middle_name">Middle Name</label>
-                            <input type="text" name="middle_name" id="middle_name" class="form-control" value="{{ $patient->middle_name }}">
+                            <input type="text" name="middle_name" id="middle_name" class="form-control"
+                                   value="{{ $patient->middle_name }}">
                         </div>
                         <div class="form-group">
                             <label for="last_name">Last Name</label>
-                            <input type="text" name="last_name" id="last_name" class="form-control" value="{{ $patient->last_name }}" required>
+                            <input type="text" name="last_name" id="last_name" class="form-control"
+                                   value="{{ $patient->last_name }}" required>
                         </div>
 
                         <h5>Kids</h5>
@@ -270,15 +288,18 @@
                             <div class="form-group">
                                 <input type="hidden" name="kid_ids[]" value="{{ $kid->id }}">
                                 <label for="kid_name_{{ $patient->id }}_{{ $index }}">Name</label>
-                                <input type="text" name="kid_names[]" id="kid_name_{{ $patient->id }}_{{ $index }}" class="form-control" value="{{ $kid->name }}">
+                                <input type="text" name="kid_names[]" id="kid_name_{{ $patient->id }}_{{ $index }}"
+                                       class="form-control" value="{{ $kid->name }}">
                             </div>
                             <div class="form-group">
                                 <label for="kid_dob_{{ $patient->id }}_{{ $index }}">Date of Birth</label>
-                                <input type="date" name="kid_dobs[]" id="kid_dob_{{ $patient->id }}_{{ $index }}" class="form-control" value="{{ $kid->dob }}">
+                                <input type="date" name="kid_dobs[]" id="kid_dob_{{ $patient->id }}_{{ $index }}"
+                                       class="form-control" value="{{ $kid->dob }}">
                             </div>
                             @endforeach
                         </div>
-                        <button type="button" class="btn btn-primary" id="addEditKidButton{{ $patient->id }}">Add Kid</button>
+                        <button type="button" class="btn btn-primary" id="addEditKidButton{{ $patient->id }}">Add Kid
+                        </button>
 
                         <h5>Adults</h5>
                         <div id="editAdultsSection{{ $patient->id }}">
@@ -286,25 +307,34 @@
                             <div class="form-group">
                                 <input type="hidden" name="adult_ids[]" value="{{ $adult->id }}">
                                 <label for="adult_first_name_{{ $patient->id }}_{{ $index }}">Name</label>
-                                <input type="text" name="adult_first_names[]" id="adult_first_name_{{ $patient->id }}_{{ $index }}" class="form-control" value="{{ $adult->first_name }}">
+                                <input type="text" name="adult_first_names[]"
+                                       id="adult_first_name_{{ $patient->id }}_{{ $index }}" class="form-control"
+                                       value="{{ $adult->first_name }}">
                             </div>
                             <div class="form-group">
                                 <input type="hidden" name="adult_ids[]" value="{{ $adult->id }}">
                                 <label for="adult_middle_name_{{ $patient->id }}_{{ $index }}">Name</label>
-                                <input type="text" name="adult_middle_names[]" id="adult_middle_name_{{ $patient->id }}_{{ $index }}" class="form-control" value="{{ $adult->middle_name }}">
+                                <input type="text" name="adult_middle_names[]"
+                                       id="adult_middle_name_{{ $patient->id }}_{{ $index }}" class="form-control"
+                                       value="{{ $adult->middle_name }}">
                             </div>
                             <div class="form-group">
                                 <input type="hidden" name="adult_ids[]" value="{{ $adult->id }}">
                                 <label for="adult_last_name_{{ $patient->id }}_{{ $index }}">Name</label>
-                                <input type="text" name="adult_last_names[]" id="adult_last_name_{{ $patient->id }}_{{ $index }}" class="form-control" value="{{ $adult->last_name }}">
+                                <input type="text" name="adult_last_names[]"
+                                       id="adult_last_name_{{ $patient->id }}_{{ $index }}" class="form-control"
+                                       value="{{ $adult->last_name }}">
                             </div>
                             <div class="form-group">
                                 <label for="adult_dob_{{ $patient->id }}_{{ $index }}">Date of Birth</label>
-                                <input type="date" name="adult_dobs[]" id="adult_dob_{{ $patient->id }}_{{ $index }}" class="form-control" value="{{ $adult->dob }}">
+                                <input type="date" name="adult_dobs[]" id="adult_dob_{{ $patient->id }}_{{ $index }}"
+                                       class="form-control" value="{{ $adult->dob }}">
                             </div>
                             @endforeach
                         </div>
-                        <button type="button" class="btn btn-primary" id="addEditAdultButton{{ $patient->id }}">Add Adult</button>
+                        <button type="button" class="btn btn-primary" id="addEditAdultButton{{ $patient->id }}">Add
+                            Adult
+                        </button>
                         <br>
                         <button type="submit" class="btn btn-primary">Update Patient</button>
                     </form>
@@ -319,11 +349,11 @@
 
 @section('scripts')
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         var kidCount = 1;
         var adultCount = 1;
 
-        $('#addKidButton').click(function() {
+        $('#addKidButton').click(function () {
             var kidSection = `
                     <div class="form-group">
                         <label for="kid_name_${kidCount}">Name</label>
@@ -338,7 +368,7 @@
             kidCount++;
         });
 
-        $('#addAdultButton').click(function() {
+        $('#addAdultButton').click(function () {
             var adultSection = `
                     <div class="form-group">
                         <label for="adult_first_name_${adultCount}">First Name</label>
@@ -363,11 +393,21 @@
     });
 </script>
 <script>
-    $(document).ready(function() {
-        var editKidCount = {{ $patient->kids_count }};
-        var editAdultCount = {{ $patient->adults_count }};
+    $(document).ready(function () {
+        var editKidCount = {
+        {
+            $patient->kids_count
+        }
+    }
+        ;
+        var editAdultCount = {
+        {
+            $patient->adults_count
+        }
+    }
+        ;
 
-        $('#addEditKidButton{{ $patient->id }}').click(function() {
+        $('#addEditKidButton{{ $patient->id }}').click(function () {
             var kidSection = `
                     <input type="hidden" name="kid_ids[]" value="">
                     <div class="form-group">
@@ -383,7 +423,7 @@
             editKidCount++;
         });
 
-        $('#addEditAdultButton{{ $patient->id }}').click(function() {
+        $('#addEditAdultButton{{ $patient->id }}').click(function () {
             var adultSection = `
                     <input type="hidden" name="adult_ids[]" value="">
                     <div class="form-group">
@@ -409,3 +449,4 @@
     });
 </script>
 @endsection
+}
